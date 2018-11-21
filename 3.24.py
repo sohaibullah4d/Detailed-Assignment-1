29478713e0136b6faa27bb3e69333e8d6daeeca9
Muhammad Sohaib <sohaibullah5492@gmail.com>
2018-11-21 12:22:35 +0500

    Add files via upload

---
+++ 3.24.py
@@ -0,0 +1,8 @@
+print("MUHAMMAD SOHAIB - 18B-054-CS - section A")
+
+print("\t\t\t PROBLEMS: 3.24")
+lst = eval(input("Enter list of words: "))
+for x in lst:
+    if x != "secret":
+        print(x)
+    
